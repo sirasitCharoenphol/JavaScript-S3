a3567d354e900d3cb9ce846be1bcfe7a8dc2f2af
SirasitXD <s6506021621110@email.kmutnb.ac.th>
2024-12-04 11:02:33 +0700

    3

---
+++ Lecture02/arrowf-sample.js
@@ -0,0 +1,17 @@
+const sum = (firstPatam, secondParam) => {
+    return firstPatam + secondParam;
+};
+console.log(sum(2,5));
+
+const printHello = () => {
+    console.log("hello");
+};
+printHello();
+
+const checkWeight = (weight) => {
+    console.log(`Baggage weight : ${weight} kilograms.`);
+}
+checkWeight(25);
+
+const multiply = (a,b) => a * b;
+console.log(multiply(2,30));
