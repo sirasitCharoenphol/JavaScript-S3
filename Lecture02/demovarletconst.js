e2520e6c558eff0c35afefe215c95432b87a059e
SirasitXD <s6506021621110@email.kmutnb.ac.th>
2024-12-04 09:32:38 +0700

    1

---
+++ Lecture02/demovarletconst.js
@@ -0,0 +1,17 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if(true){
+    let world = "hello World";
+    console.log(world);
+}
+// console.log(world);
+
+const fixval = "Fix Value";
+console.log(fixval);
+
+
+// fixvar = "Tes assigne value to const"
---
+++ Lecture02/numconv.js
@@ -0,0 +1,25 @@
+let num1 = '150';
+let flo1 = '1.50';
+
+console.log("*******Converting strings to integers***********");
+console.log(parseInt('100'));
+console.log(parseInt(num1));
+console.log(parseInt('ABC'));
+console.log(parseInt(0xF));
+
+console.log("**********Coverting to float**********");
+console.log(parseFloat('1.25abc'));
+console.log(parseFloat(flo1));
+console.log(parseFloat('ABC'));
+
+console.log("******Monre Conversion Examples***********");
+
+console.log(parseInt('1.5'));
+console.log(parseInt('1 + 1'));
+
+console.log(parseInt(`${1 + 1}`));
+
+console.log("*********Conversion numbers to strings***********");
+console.log(num1.toString());
+console.log(flo1.toString());
+console.log((100).toString());
